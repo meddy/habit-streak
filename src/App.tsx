b5096abc6a8f4de3967844aac8dabc0b601a97e3
Max Eddy <maxiscool@gmail.com>
2020-04-13 20:08:00 -0700

    Add fontawesome.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
-import { Container, ListGroup, ListGroupItem } from "react-bootstrap";
+import { Button, Container, ListGroup, ListGroupItem } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import styles from "./App.module.css";
 
 function App() {
   return (
     <Container className={styles.container}>
       <ListGroup>
-        <ListGroupItem>Read a Book</ListGroupItem>
+        <ListGroupItem className="d-flex justify-content-between">
+          <span>Read a Book</span>
+          <Button variant="danger" size="sm">
+            <FontAwesomeIcon icon={faTrash} />
+          </Button>
+        </ListGroupItem>
         <ListGroupItem>Study Spanish</ListGroupItem>
         <ListGroupItem>Stretch 20 mins</ListGroupItem>
       </ListGroup>
