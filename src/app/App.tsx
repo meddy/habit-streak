5a57cbabbdcb566711a76786d5082aa5445c43ad
Max Eddy <maxiscool@gmail.com>
2020-05-10 18:01:54 -0700

    Add navbar.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import HomePage from "../page/HomePage";
@@ -9,6 +9,16 @@ import styles from "./App.module.css";
 export default function App() {
   return (
     <BrowserRouter>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Habit Streak</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+          </Nav>
+          <Button variant="primary">Log In</Button>
+        </Container>
+      </Navbar>
       <Container className={styles.container}>
         <Switch>
           <Route path="/" exact>
--- src/page/HomePage.tsx
+++ src/page/HomePage.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Col, Row } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 
 import { RootState } from "../app/store";
@@ -11,23 +10,19 @@ export default function HomePage() {
   const dispatch = useDispatch();
   const habits = useSelector((state: RootState) => state.habits);
 
+  // Nav bar, add about and login
+  // move habit form under
+  // implement email link login
   return (
     <>
-      <Row>
-        <Col md={12} lg={6}>
-          <h1>Habit Streak</h1>
-        </Col>
-        <Col>
-          <HabitForm
-            existing={habits}
-            submitLabel="Add Habit"
-            onSubmit={(newHabit) => {
-              dispatch(addHabit(newHabit));
-              return "";
-            }}
-          />
-        </Col>
-      </Row>
+      <HabitForm
+        existing={habits}
+        submitLabel="Add Habit"
+        onSubmit={(newHabit) => {
+          dispatch(addHabit(newHabit));
+          return "";
+        }}
+      />
       <HabitList habits={habits} />
     </>
   );
