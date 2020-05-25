b0f0d3b00b22ca5cdf895457cfdd7f5c6f7df6c8
Max Eddy <maxiscool@gmail.com>
2020-05-24 20:16:48 -0700

    Setup user state.

---
+++ src/slices/userSlice.ts
@@ -0,0 +1,54 @@
+import {
+  createAsyncThunk,
+  createSlice,
+  SerializedError,
+} from "@reduxjs/toolkit";
+import firebase from "firebase";
+
+interface UserSliceState {
+  email: string | null;
+  error?: SerializedError | null;
+  pending: boolean;
+}
+
+interface SendEmailLinkPayload {
+  email: string;
+  url: string;
+}
+
+const sendEmailLink = createAsyncThunk(
+  "user/sendEmailLink",
+  async (payload: SendEmailLinkPayload, thunkAPI) => {
+    const { email, url } = payload;
+    await firebase
+      .auth()
+      .sendSignInLinkToEmail(email, { url, handleCodeInApp: true });
+    return email;
+  }
+);
+
+const initialState: UserSliceState = {
+  email: null,
+  error: null,
+  pending: false,
+};
+
+const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {},
+  extraReducers: {
+    [sendEmailLink.pending.type]: (state) => {
+      state.pending = true;
+    },
+    [sendEmailLink.fulfilled.type]: (state, action) => {
+      state.email = action.payload;
+    },
+    [sendEmailLink.rejected.type]: (state, action) => {
+      state.pending = false;
+      state.error = action.error;
+    },
+  },
+});
+
+export default userSlice;
